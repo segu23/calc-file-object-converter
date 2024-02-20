e542694be985366ff2fb9783b1c30a74323f6a2f
Segundo Schmidt <segundo.s@autoplants.com.ar>
2024-02-20 14:45:45 -0300

    big refactor

--- src/main/java/com/github/segu23/xlsxconverter/controller/DocumentController.java
+++ src/main/java/com/github/segu23/xlsxconverter/controller/DocumentController.java
@@ -1,13 +1,10 @@
 package com.github.segu23.xlsxconverter.controller;
 
-import com.github.segu23.xlsxconverter.exception.InvalidFileTypeException;
 import com.github.segu23.xlsxconverter.exception.SheetNotFoundException;
 import com.github.segu23.xlsxconverter.model.SecondTestUserModel;
 import com.github.segu23.xlsxconverter.model.TestUserModel;
-import com.github.segu23.xlsxconverter.xlsx.XlsxConverter;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import com.github.segu23.xlsxconverter.CalcFileConverter;
 import org.apache.poi.ss.usermodel.*;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.*;
 
 @RestController
@@ -29,18 +25,23 @@ public class DocumentController {
             @RequestParam("startColumn") int startColumn,
             @RequestParam(value = "sheet", required = false) String sheetName,
             @RequestParam(value = "sheetIndex", required = false, defaultValue = "0") int sheetIndex) {
-
         try{
-            Sheet sheet = getSheet(file, sheetName, sheetIndex);
+            Workbook workbook = CalcFileConverter.getWorkbook(file);
+            Sheet sheet;
 
-            int endRow = XlsxConverter.getLastRow(sheet, startRow, startColumn);
-            System.out.println("EndRow: " + endRow);
-            int endColumn = XlsxConverter.getLastColumn(sheet, startRow, startColumn);
-            System.out.println("EndColumn: " + endColumn);
+            if(sheetName != null && !sheetName.equalsIgnoreCase("")){
+                sheet = CalcFileConverter.getSheetByName(workbook, sheetName);
+            }else{
+                sheet = CalcFileConverter.getSheetByIndex(workbook, sheetIndex);
+            }
 
-            List<TestUserModel> users = XlsxConverter.extractObjectsFromTable(sheet, startRow, endRow, startColumn, endColumn, TestUserModel.class);
+            if(sheet == null) throw new SheetNotFoundException();
+
+            int endRow = CalcFileConverter.getLastRow(sheet, startRow, startColumn);
+            int endColumn = CalcFileConverter.getLastColumn(sheet, startRow, startColumn);
+
+            List<TestUserModel> users = CalcFileConverter.extractObjectsFromTable(sheet, startRow, endRow, startColumn, endColumn, TestUserModel.class);
             users.forEach(System.out::println);
-
 
             return ResponseEntity.ok().build();
         }catch (Exception e){
@@ -53,16 +54,24 @@ public class DocumentController {
     public ResponseEntity<?> uploadDocument2(
             @RequestParam("file") MultipartFile file,
             @RequestParam("startRow") int startRow,
-            @RequestParam("endRow") int endRow,
             @RequestParam("startColumn") int startColumn,
-            @RequestParam("endColumn") int endColumn,
             @RequestParam(value = "sheet", required = false) String sheetName,
             @RequestParam(value = "sheetIndex", required = false, defaultValue = "0") int sheetIndex) {
-
         try{
-            Sheet sheet = getSheet(file, sheetName, sheetIndex);
+            Workbook workbook = CalcFileConverter.getWorkbook(file);
+            Sheet sheet;
 
-            List<SecondTestUserModel> users = XlsxConverter.extractObjectsFromTable(sheet, startRow, endRow, startColumn, endColumn, SecondTestUserModel.class);
+            if(sheetName != null && !sheetName.equalsIgnoreCase("")){
+                sheet = CalcFileConverter.getSheetByName(workbook, sheetName);
+            }else{
+                sheet = CalcFileConverter.getSheetByIndex(workbook, sheetIndex);
+            }
+
+            if(sheet == null) throw new SheetNotFoundException();
+            int endRow = CalcFileConverter.getLastRow(sheet, startRow, startColumn);
+            int endColumn = CalcFileConverter.getLastColumn(sheet, startRow, startColumn);
+
+            List<SecondTestUserModel> users = CalcFileConverter.extractObjectsFromTable(sheet, startRow, endRow, startColumn, endColumn, SecondTestUserModel.class);
             users.forEach(System.out::println);
 
             return ResponseEntity.ok().build();
@@ -70,29 +79,5 @@ public class DocumentController {
             e.printStackTrace();
         }
         return ResponseEntity.badRequest().build();
-    }
-
-    private static Sheet getSheet(MultipartFile file, String sheetName, int sheetIndex) throws IOException, InvalidFileTypeException, SheetNotFoundException {
-        Workbook workbook;
-
-        if(file.getOriginalFilename().endsWith(".xlsx")){
-            workbook = new XSSFWorkbook(file.getInputStream());
-        }else if(file.getOriginalFilename().endsWith(".xlsx")){
-            workbook = new HSSFWorkbook(file.getInputStream());
-        }else{
-            throw new InvalidFileTypeException();
-        }
-
-        Sheet sheet;
-
-        if(sheetName != null && !sheetName.equalsIgnoreCase("")){
-            sheet = workbook.getSheet(sheetName);
-        }else{
-            sheet = workbook.getSheetAt(sheetIndex);
-        }
-
-        if(sheet == null) throw new SheetNotFoundException();
-
-        return sheet;
     }
 }
